1490667b05ba36e141ea0df496eb2cbef741e1ae
Engywook2005 <uig6fl*ht1ft>
2020-04-21 23:01:38 -0400

    Have container working with public_html and have node working for sql interface middleware. No harm done on the sql side except I need to repopulate the database.

--- sqlMiddleware/src/server/index.js
+++ sqlMiddleware/src/server/index.js
@@ -3,7 +3,7 @@
  */
 
 const NodeJSDataServer = require('./node-js-data-server');
-const HTTPServer = require('http-server');
+const HTTPServer = require('sqlMiddleware/src/server/http-server');
 
 module.exports.NodeJSDataServer = NodeJSDataServer;
 module.exports.HTTPServer = HTTPServer;
